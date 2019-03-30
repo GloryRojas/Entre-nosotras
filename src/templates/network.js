23287d0012d0f3217170de3f3455bd0924189ae2
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-03-30 11:36:54 -0500

    Footer para patantalla principal

--- src/templates/network.js
+++ src/templates/network.js
@@ -8,15 +8,23 @@ export const menuNavegacionHome = () => {
   const pantallaPrincipal = `
     <header>
       <div class="contenedor">
-            <img class="logo" src='img/logo-home.png' alt=""><h1 class="bienvenidx">Entre Nosotras</h1>
-            <input type="checkbox"  id="btn-menu"><label for="btn-menu" class="icon-menu"></label>
-            <nav class="menu">
-                <a href="#/home" id="btn-inicio"><i class="fas fa-home"></i></a>
-                <a href="#/myprofile" id="btn-miperfil"><i class="fas fa-phone-volume"></i></a>
-                <a href="#/login" id="btn-cerrar-sesion"><i class="fas fa-home"></i></a>     
-            </nav>
+        <input type="checkbox"  id="btn-menu"><label for="btn-menu" class="icon-menu"><i class="fas fa-bars"></i></label>
+        <h1> Entre nosotras</h1>
+        <a href='#home'><i class="far fa-comments icon"></i></a>
+        <nav class="menu">
+          <a href="#/home" id="btn-inicio">Configuración de cuenta</a>
+          <a href="#/myprofile" id="btn-miperfil">Acerca de nosotras</a>
+          <a href="#/login" id="btn-cerrar-sesion">Salir</a>     
+        </nav>
       </div>
     </header>
+    <footer>
+      <nav>
+        <a href="#/home" id="btn-inicio">hola</a>
+        <a href="#/myprofile" id="btn-miperfil"></a>
+        <a href="#/login" id="btn-cerrar-sesion"></a>     
+      </nav>
+    </footer>
   `;
   headerHome.innerHTML = pantallaPrincipal;
 
